272c3a7c8d9a907a78af6264dfe26c73a6796ade
amirrezaask <raskarpour@gmail.com>
2025-04-04 00:14:14 +0330

    Work In Progress 2025-04-04 00:14:14

--- lua/nvim-fuzzy/init.lua
+++ lua/nvim-fuzzy/init.lua
@@ -24,16 +24,13 @@ function Fuzzy.new(opts)
 	assert(type(opts.transformer) == "function", "opts.transformer must be a function")
 	assert(opts.actions, "opts.actions is required")
 	assert(type(opts.actions) == "table", "opts.actions must be a table")
+
 	opts.query = opts.query or ""
+
 	opts.sort = function(opts)
 		return sort_by_string_distance(opts.query, opts.results)
 	end
 
-	---@type Fuzzy.Opts
-	opts.results = {}
-	opts.buf_lines = {}
-
-	opts.last_query = ""
 	local row = math.floor(vim.o.lines * 0.3)
 	local col = math.floor(vim.o.columns * 0.5)
 	local buf = vim.api.nvim_create_buf(false, true)
@@ -48,13 +45,7 @@ function Fuzzy.new(opts)
 		border = "rounded",
 	})
 
-	local ns = vim.api.nvim_create_namespace("nvim-fuzzy")
-
-	local function update()
-		local start = vim.loop.hrtime()
-		opts.source(query, opts)
-		local source_elapsed = (vim.loop.hrtime() - start) / 1e6
-
+	function opts.update(opts)
 		start = vim.loop.hrtime()
 		opts.results = opts.sort(opts)
 		local sort_elapsed = (vim.loop.hrtime() - start) / 1e6
@@ -68,20 +59,26 @@ function Fuzzy.new(opts)
 			end
 		end
 
-		table.insert(opts.buf_lines, opts.query)
-		opts.last_query = opts.query
-		if not opts.selected_item then
-			opts.selected_item = #opts.results - 2
-		end
-		print(string.format("%x %f", #opts.results, sort_elapsed))
+		table.insert(opts.buf_lines, opts.query) -- adds a line for prompt
+		opts:draw()
 	end
 
-	local function draw()
+	function opts.draw(opts)
+		if not opts.selected_item then
+			opts.selected_item = 1
+		end
+
+		if opts.selected_item < 0 then
+			opts.selected_item = #opts.results - 2
+		end
+
+		if opts.selected_item > #opts.results - 2 then
+			opts.selected_item = 0
+		end
 		if #opts.buf_lines == 0 then
 			print("No results, return")
-			return
 		end
-		vim.api.nvim_buf_clear_namespace(buf, ns, 0, -1)
+		vim.api.nvim_buf_clear_namespace(buf, opts.hl_ns, 0, -1)
 		---@type string[]
 
 		if not opts.buf_lines or #opts.buf_lines == 0 then
@@ -91,75 +88,77 @@ function Fuzzy.new(opts)
 
 		vim.api.nvim_win_set_cursor(win, { #opts.buf_lines, #opts.query + 1 })
 
-		if opts.selected_item < 0 then
-			opts.selected_item = #opts.results - 2
-		end
+		-- print(string.format("%x %f", #opts.results, opts.source_elapsed or 0))
+		print(opts.selected_item, opts.query)
+		vim.hl.range(buf, opts.hl_ns, "Question", { opts.selected_item, 0 }, { opts.selected_item, width })
+	end
 
-		if opts.selected_item > #opts.results - 2 then
-			opts.selected_item = #opts.results - 2
-		end
+	function opts.set_keymaps(opts)
+		vim.keymap.set({ "n", "i" }, "<C-n>", function()
+			opts.selected_item = opts.selected_item + 1
+			opts:draw()
+		end, { buffer = buf })
 
-		-- print(
-		-- 	"Count",
-		-- 	#opts.results,
-		-- 	-- "selected",
-		-- 	-- selected_item,
-		-- 	-- "",
-		-- 	-- opts.last_results_came,
-		-- 	"Cost",
-		-- 	opts.last_results_sorted
-		-- )
-		vim.hl.range(buf, ns, "Question", { opts.selected_item, 0 }, { opts.selected_item, width })
+		vim.keymap.set({ "n", "i" }, "<C-p>", function()
+			opts.selected_item = opts.selected_item - 1
+			opts:draw()
+		end, { buffer = buf })
+
+		vim.keymap.set({ "n", "i" }, "<CR>", function()
+			local item = opts.results[opts.selected_item + 2]
+			opts.actions.enter(item)
+		end, { buffer = buf })
+	end
+
+	function opts.setup_autocmds(opts)
+		vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+			buffer = buf,
+			callback = function()
+				local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+				opts.query = lines[#opts.buf_lines]
+				if #lines < #opts.buf_lines then
+					opts.query = ""
+				end
+				if opts.query ~= opts.last_query or not opts.initialized then
+					opts.last_query = opts.query
+					opts:source()
+				end
+			end,
+		})
 	end
 
 	-- Initialize state
-	update()
-	draw()
+	opts.results = {}
+	opts.buf_lines = {}
+	opts.last_query = ""
+	opts.hl_ns = vim.api.nvim_create_namespace("nvim-fuzzy")
 
-	vim.keymap.set({ "n", "i" }, "<C-n>", function()
-		opts.selected_item = opts.selected_item + 1
-		draw()
-	end, { buffer = buf })
-
-	vim.keymap.set({ "n", "i" }, "<C-p>", function()
-		opts.selected_item = opts.selected_item - 1
-		draw()
-	end, { buffer = buf })
-
-	vim.keymap.set({ "n", "i" }, "<CR>", function()
-		local item = opts.results[opts.selected_item]
-		opts.actions.enter(item)
-	end, { buffer = buf })
-
-	vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
-		buffer = buf,
-		callback = function()
-			local lines = vim.api.nvim_buf_get_lines(buf, -2, -1, false)
-			opts.query = lines[1]
-			if opts.query ~= opts.last_query or not opts.initialized then
-				update()
-				draw()
-			end
-		end,
-	})
+	opts:set_keymaps()
+	opts:setup_autocmds()
+	opts:source()
 end
 local find = require("nvim-fuzzy.find")
 
-local files_fetched = false
 Fuzzy.new({
-	source = function(query, opts)
+	files_fetched = false,
+	source = function(opts)
 		local start = vim.loop.hrtime()
-		if not files_fetched then
+		if not opts.files_fetched then
 			find(vim.fn.expand("~/.dotfiles"), function(res)
 				local results_came = (vim.loop.hrtime() - start) / 1e6
 				opts.results = {}
 				for _, v in ipairs(res) do
 					table.insert(opts.results, make_entry(v))
 				end
-				files_fetched = true
+				opts.source_elapsed = (vim.loop.hrtime() - start) / 1e6
+				opts.files_fetched = true
+				vim.schedule(function()
+					opts:update()
+				end)
 			end)
+		else
+			opts:update()
 		end
-		opts.source_tool = (vim.loop.hrtime() - start) / 1e6
 	end,
 
 	---@param e Fuzzy.Entry
